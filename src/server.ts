64716b26c734d4b1353144845c01b4015e81c4e5
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-11-13 11:45:51 -0300

    Configurando eslint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 const app = express();
-const PORT = "3333"
-app.get("/",(req,res)=>{
-  res.json({message:"hello world"})
-})
-app.listen(PORT,()=>console.log(`http://localhost:${PORT}`)
-)
+const port = '3333';
+app.get('/', (req, res) => {
+	res.json({message: 'hello world'});
+});
+app.listen(port, () => {
+	console.log(`http://localhost:${port}`);
+},
+);
