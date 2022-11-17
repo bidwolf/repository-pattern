7a5ff3b252f5f1d64e5ea0fc3c8d3d2cebc584de
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-11-17 10:18:33 -0300

    fix create overwrite

--- src/modules/cars/repositories/repository.ts
+++ src/modules/cars/repositories/repository.ts
@@ -3,3 +3,11 @@ export abstract class Repository<T> implements BaseRepository<T> {
 	public abstract create(data: T): void;
 	public abstract list(): T[];
 }
+type A = {
+	name: string;
+};
+const a = {name: 'asklhadslk'};
+const b = {a};
+function askhdkadjs(a: a) {
+	console.log(a.name);
+}
