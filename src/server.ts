2d3d7330fd115345e20fe836eba9fc631f3f8e71
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-11-17 18:14:49 -0300

    Adicionando documentação automaticamente utilizando o swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {router} from './routes';
+import swaggerUI from 'swagger-ui-express';
+import swaggerConfig from './swagger.json';
 const app = express();
 const port = process.env.port ?? '3333';
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 app.use(router);
 app.listen(port, () => {
 	console.log(`http://localhost:${port}`);
