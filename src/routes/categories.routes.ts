08093b67147ba5b8997167e386d267a210c8acf0
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-11-17 11:51:31 -0300

    Adicionando rota de import para specifications e utilizando middleware de upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,11 @@
 import {Router as router} from 'express';
 import {createCategoryController} from '../modules/cars/useCases/createCategory';
 import {listCategoriesController} from '../modules/cars/useCases/listCategories';
-import {ImportCategoryController} from '../modules/cars/useCases/importCategory/importCategoryController';
-import multer from 'multer';
 import {importCategoryController} from '../modules/cars/useCases/importCategory';
+import {uploadMiddleware} from '../modules/cars/middlewares/upload';
 const categoriesRoutes = router();
-const upload = multer({
-	dest: './tmp',
-});
+
 categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
 categoriesRoutes.get('/', (request, response) => listCategoriesController.handle(request, response));
-categoriesRoutes.post('/import', upload.single('file'), (request, response) => importCategoryController.handle(request, response));
+categoriesRoutes.post('/import', uploadMiddleware, async (request, response) => importCategoryController.handle(request, response));
 export {categoriesRoutes};
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,6 +1,9 @@
 import {Router as router} from 'express';
+import {uploadMiddleware} from '../modules/cars/middlewares/upload';
 import {createSpecificationController} from '../modules/cars/useCases/createSpecification';
+import {importSpecificationController} from '../modules/cars/useCases/importSpecification';
 import {listSpecificationsController} from '../modules/cars/useCases/listSpecifications';
 export const specificationsRoutes = router();
 specificationsRoutes.post('/', (request, response) => createSpecificationController.handle(request, response));
 specificationsRoutes.get('/', (request, response) => listSpecificationsController.handle(request, response));
+specificationsRoutes.post('/import', uploadMiddleware, async (request, response) => importSpecificationController.handle(request, response));
